7184dd867b1e7e9aac863cf7a69b449de4dd3c5b
Rob Latour <5200730+roblatour@users.noreply.github.com>
2026-09-23 00:08:20 -0400

    Create rgb_led_colours.c

---
+++ main/custom/rgb_led_colours.c
@@ -0,0 +1,15 @@
+// ESP32 Time Server
+// Copyright Rob Latour, 2026
+// License: MIT
+// Website: https://github.com/roblatour/ESP32TimeServer
+//
+
+enum class RGB_LED_Colour : uint8_t
+{
+    off,
+    red,
+    green,
+    blue,
+    yellow,
+    white
+};
